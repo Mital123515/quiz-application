5fefddcf8973797a69caf4bd7c079543908a1f73
SidraTaranum <71249763+SidraTaranum@users.noreply.github.com>
2023-05-11 21:26:29 +0100

    Added questions

--- backend/database/data.js
+++ backend/database/data.js
@@ -1,229 +1,190 @@
 export default [
     {
-        id: 1,
-        question : "Javascript is an _______ language",
-        options : [
-            'Object-Oriented',
-            'Object-Based',
-            'Procedural',
-        ]
+      id: 1,
+      question: "Javascript is an _______ language",
+      options: ["Object-Oriented", "Object-Based", "Procedural"],
     },
     {
-        id: 2,
-        question : "Following methods can be used to display data in some form using Javascript",
-        options : [
-            'document.write()',
-            'console.log()',
-            'window.alert()',
-        ]
+      id: 2,
+      question:
+        "Following methods can be used to display data in some form using Javascript",
+      options: ["document.write()", "console.log()", "window.alert()"],
     },
     {
-        id: 3,
-        question : "When an operator value is NULL, the typeof returned by the unary operator is:",
-        options : [
-            'Boolean',
-            'Undefined',
-            'Object',
-        ]
+      id: 3,
+      question:
+        "When an operator value is NULL, the typeof returned by the unary operator is:",
+      options: ["Boolean", "Undefined", "Object"],
     },
     {
-        id: 4,
-        question : "What does the toString() method return?",
-        options : [
-            'Return Object',
-            'Return String',
-            'Return Integer'
-        ]
+      id: 4,
+      question: "What does the toString() method return?",
+      options: ["Return Object", "Return String", "Return Integer"],
     },
     {
-        id: 5,
-        question : "Which function is used to serialize an object into a JSON slice?",
-        options : [
-            'stringify()',
-            'parse()',
-            'convert()',
-        ]
+      id: 5,
+      question:
+        "Which function is used to serialize an object into a JSON string?",
+      options: ["stringify()", "parse()", "convert()"],
     },
     {
-        id: 6,
-        question : "What is the first step in the software development lifecycle?",
-        options : [
-            'System Design',
-            'Coding',
-            'Preliminary Investigation and Analysis',
-        ]
+      id: 6,
+      question: "What does the study of an existing system refer to?",
+      options: ["System Analysis", "Details of DFD", "Feasibility Study"],
     },
     {
-        id: 7,
-        question : "Following methods can be used to display data in some form using Javascript",
-        options : [
-            'document.write()',
-            'console.log()',
-            'window.alert()',
-        ]
+      id: 7,
+      question: "What does RAD stand for?",
+      options: [
+        "Rapid Application Document",
+        "Rapid Application Development",
+        "Relative Application Development",
+      ],
     },
     {
-        id: 8,
-        question : "When an operator value is NULL, the typeof returned by the unary operator is:",
-        options : [
-            'Boolean',
-            'Undefined',
-            'Object',
-        ]
+      id: 8,
+      question:
+        "Which of the following prototypes does not associated with Prototyping Model?",
+      options: ["Domain Prototype", "Horizontal Prototype", "Diagonal Prototype"],
     },
     {
-        id: 9,
-        question : "What does the toString() method return?",
-        options : [
-            'Return Object',
-            'Return String',
-            'Return Integer'
-        ]
+      id: 9,
+      question: "What is the major drawback of the Spiral Model?",
+      options: [
+        "Doesnot work well for smaller projects",
+        "Additional functionalities are added later on",
+        "Strong approval and documentation control",
+      ],
     },
     {
-        id: 10,
-        question : "Which function is used to serialize an object into a JSON string?",
-        options : [
-            'stringify()',
-            'parse()',
-            'convert()',
-        ]
+      id: 10,
+      question:
+        "Which of the following models doesn't necessitate defining requirements at the earliest in the lifecycle?",
+      options: [
+        "Spiral & Prototyping",
+        "Prototyping & Waterfall",
+        "RAD & Waterfall",
+      ],
     },
     {
-        id: 11,
-        question : "Javascript is an _______ language",
-        options : [
-            'Object-Oriented',
-            'Object-Based',
-            'Procedural',
-        ]
+      id: 11,
+      question:
+        "Which of the following is an example of Black Box and Functional Processing?",
+      options: [
+        "Fourth Generation Language",
+        "Third Generation Language",
+        "Second Generation Language",
+      ],
     },
     {
-        id: 12,
-        question : "Following methods can be used to display data in some form using Javascript",
-        options : [
-            'document.write()',
-            'console.log()',
-            'window.alert()',
-        ]
+      id: 12,
+      question: "__________ is identified as fourth generation language.",
+      options: ["C++", "Unix shell", "COBOL"],
     },
     {
-        id: 13,
-        question : "When an operator value is NULL, the typeof returned by the unary operator is:",
-        options : [
-            'Boolean',
-            'Undefined',
-            'Object',
-        ]
+      id: 13,
+      question:
+        " Which of the following falls under the category of software products?",
+      options: ["Firmware, CAD", "Embedded, CAM", "Customized, Generic"],
     },
     {
-        id: 14,
-        question : "What does the toString() method return?",
-        options : [
-            'Return Object',
-            'Return String',
-            'Return Integer'
-        ]
+      id: 14,
+      question:
+        "Which of the following activities of the generic process framework delivers a feedback report?",
+      options: ["Planning", "Deployment", "Construction"],
     },
     {
-        id: 15,
-        question : "Which function is used to serialize an object into a JSON string?",
-        options : [
-            'stringify()',
-            'parse()',
-            'convert()',
-        ]
+      id: 15,
+      question: " Which of the following refers to internal software equality?",
+      options: ["Reusability", "Reliability", "Usability"],
     },
     {
-        id: 16,
-        question : "Javascript is an _______ language",
-        options : [
-            'Object-Oriented',
-            'Object-Based',
-            'Procedural',
-        ]
+      id: 16,
+      question:
+        " RUP is abbreviated as __________, invented by a division of __________.",
+      options: [
+        "Rational Unified Process, IBM",
+        "Rational Unified Program, IBM",
+        "Rational Unified Program, Infosys",
+      ],
     },
     {
-        id: 17,
-        question : "Following methods can be used to display data in some form using Javascript",
-        options : [
-            'document.write()',
-            'console.log()',
-            'window.alert()',
-        ]
+      id: 17,
+      question:
+        " Which one of the following activities is not recommended for software processes in software engineering?",
+      options: [
+        "Software Evolution",
+        "Software Verification",
+        "Software designing",
+      ],
     },
     {
-        id: 18,
-        question : "When an operator value is NULL, the typeof returned by the unary operator is:",
-        options : [
-            'Boolean',
-            'Undefined',
-            'Object',
-        ]
+      id: 18,
+      question:
+        "The agile software development model is built based on __________.",
+      options: [
+        "Incremental Development",
+        "Iterative Development",
+        "Both Incremental and Iterative Development",
+      ],
     },
     {
-        id: 19,
-        question : "What does the toString() method return?",
-        options : [
-            'Return Object',
-            'Return String',
-            'Return Integer'
-        ]
+      id: 19,
+      question:
+        "Which of the following activities is not applicable to agile software development?",
+      options: [
+        "Producing only the essential work products.",
+        "Abolishing the project planning and testing.",
+        "Utilizing the strategy of incremental product delivery.",
+      ],
     },
     {
-        id: 20,
-        question : "Which function is used to serialize an object into a JSON string?",
-        options : [
-            'stringify()',
-            'parse()',
-            'convert()',
-        ]
+      id: 20,
+      question:
+        "The __________ model helps in representing the system's dynamic behavior.",
+      options: ["Behavioral Model", "Context Model", "Data Model"],
     },
     {
-        id: 21,
-        question : "Javascript is an _______ language",
-        options : [
-            'Object-Oriented',
-            'Object-Based',
-            'Procedural',
-        ]
+      id: 21,
+      question:
+        " The __________ and __________ are the two major dimensions encompassed in the Spiral model.",
+      options: [
+        "Radial, Angular",
+        "Diagonal, Perpendicular",
+        "Perpendicular, Radial",
+      ],
     },
     {
-        id: 22,
-        question : "Following methods can be used to display data in some form using Javascript",
-        options : [
-            'document.write()',
-            'console.log()',
-            'window.alert()',
-        ]
+      id: 22,
+      question:
+        "Which of the following technique is involved in certifying the sustained development of legacy systems?",
+      options: [
+        "Reverse engineering",
+        "Reverse engineering and Reengineering",
+        "Reengineering",
+      ],
     },
     {
-        id: 23,
-        question : "When an operator value is NULL, the typeof returned by the unary operator is:",
-        options : [
-            'Boolean',
-            'Undefined',
-            'Object',
-        ]
+      id: 23,
+      question:
+        "An erroneous system state that results in an unexpected system behavior is acknowledged as?",
+      options: ["System fault", "System error", "Human error or mistake"],
     },
     {
-        id: 24,
-        question : "What does the toString() method return?",
-        options : [
-            'Return Object',
-            'Return String',
-            'Return Integer'
-        ]
+      id: 24,
+      question:
+        "What is the name of the approach that follows step-by-step instructions for solving a problem?",
+      options: ["An Algorithm", "A Plan", "A List"],
     },
     {
-        id: 25,
-        question : "Which function is used to serialize an object into a JSON string?",
-        options : [
-            'stringify()',
-            'parse()',
-            'convert()',
-        ]
-    }
-];
-
-export const answers = [0, 1, 2, 1, 0,0, 1, 2, 1, 0,0, 1, 2, 1, 0,0, 1, 2, 1, 0,0, 1, 2, 1, 0];
+      id: 25,
+      question:
+        "Which of the following word correctly summarized the importance of software design?",
+      options: ["Quality", "Complexity", "Accuracy"],
+    },
+  ];
+  
+  export const answers = [
+    0, 1, 2, 1, 0, 0, 1, 2, 1, 0, 0, 1, 2, 1, 0, 0, 1, 2, 1, 0, 0, 1, 2, 1, 0,
+  ];
+  
